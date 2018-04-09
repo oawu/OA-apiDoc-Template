dc0a3375730706a1950359bac3b2c6ef55cede6b
OA Wu <comdan66@gmail.com>
2018-04-09 18:25:57 +0800

    Fix bug.

--- js/public.js
+++ js/public.js
@@ -172,9 +172,6 @@ $(function () {
                       $('<div />').addClass ('tabs').append (main.$tabs)).append (
                       $('<div />').addClass ('panels').append (main.$panels));
 
-      // main.$tabs.first ().click ();
-
-
       if (params.val.t.length && main.$tabs.filter ('[data-name="' + params.val.t + '"]').length)
         main.$tabs.filter ('[data-name="' + params.val.t + '"]').first ().click ();
       else
@@ -277,4 +274,62 @@ $(function () {
   load.init ();
   main.init ();
   menu.init ();
+
+  $(window).keydown (function (e) {
+
+    var i = parseInt (main.$format.attr ('data-i'));
+    var l = main.$tabs.length;
+
+    if (e.keyCode == 38) {
+      if (i - 1 != 0)
+        return;
+
+      var $tmp = menu.$links.filter ('.active').prev ();
+      if ($tmp.length)
+        return $tmp.click ();
+
+      $tmp = menu.$links.filter ('.active').parent ().prev ();
+      if ($tmp.length)
+        return $tmp.find ('>*').last ().click ();
+
+      $tmp = menu.$apis.find ('>*').last ();
+      return $tmp.find ('>*').last ().click ();
+    }
+    if (e.keyCode == 40) {
+      if (i - 1 != 0)
+        return;
+
+      var $tmp = menu.$links.filter ('.active').next ();
+      if ($tmp.length)
+        return $tmp.click ();
+      
+      $tmp = menu.$links.filter ('.active').parent ().next ();
+      if ($tmp.length)
+        return $tmp.find ('>*').first ().click ();
+
+
+      $tmp = menu.$apis.find ('>*').first ();
+      return $tmp.find ('>*').first ().click ();
+    }
+
+    if (e.keyCode == 39) {
+      i = i + 1;
+      i = i > l ? l : i;
+
+      menu.$el.removeClass ('focus');
+      main.$format.attr ('data-i', i);
+      params.update ('t', main.$tabs.eq (i - 1).attr ('data-name'));
+    }
+
+    if (e.keyCode == 37) {
+      i = i - 1;
+      if (i < 1) {
+        i = 0;
+        menu.$el.addClass ('focus');
+      } else {
+        main.$format.attr ('data-i', i);
+        params.update ('t', main.$tabs.eq (i - 1).attr ('data-name'));
+      }
+    }
+  });
 });
